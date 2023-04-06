bb1b16da28a9e739286f61785a060d2e92edd41f
Nguyen Anh Khoa <oggyanhkhoa123@gmail.com>
2023-04-06 10:17:08 +0700

    update home ui

--- lib/Screens/Home/home_screen.dart
+++ lib/Screens/Home/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter/material.dart';
+import 'package:note_app/screens/Home/widgets/note_widget.dart';
 import '../../models/note.dart';
 import 'detail_screen.dart';
 
@@ -13,6 +15,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final _auth = FirebaseAuth.instance;
+  bool listView = true;
   //   FirebaseFirestore.instance
   //     .collection('notes')
   //     .doc(value.user!.uid)
@@ -40,6 +43,14 @@ class _HomeScreenState extends State<HomeScreen> {
           actions: [
             IconButton(
               onPressed: () {
+                setState(() {
+                  listView = !listView;
+                });
+              },
+              icon: Icon(listView ? Icons.grid_view : Icons.list),
+            ),
+            IconButton(
+              onPressed: () {
                 _auth.signOut();
               },
               icon: const Icon(Icons.logout),
@@ -55,21 +66,13 @@ class _HomeScreenState extends State<HomeScreen> {
             if (snapshot.hasData) {
               final data = snapshot.data!.data() as Map<String, dynamic>;
               final notes = data['user_notes'] as List<dynamic>;
-              return ListView.builder(
-                itemCount: notes.length,
-                itemBuilder: (context, index) {
-                  final note = notes[index] as Map<String, dynamic>;
-                  return GestureDetector(
-                    onTap:() {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => NoteDetailScreen(note: Note.fromFirestore(note))));
-                    },
-                    child: ListTile(
-                      title: Text(note['title']),
-                      subtitle: Text(note['content']),
-                    ),
-                  );
-                },
-              );
+              return notes.isEmpty
+                  ? const Center(
+                      child: Text('Add some notes'),
+                    )
+                  : listView
+                      ? NoteListView(notes: notes)
+                      : NoteGridView(notes: notes);
             }
             return const Center(child: CircularProgressIndicator());
           },
@@ -80,5 +83,66 @@ class _HomeScreenState extends State<HomeScreen> {
           },
           child: const Icon(Icons.add),
         ));
+  }
+}
+
+class NoteListView extends StatelessWidget {
+  const NoteListView({
+    super.key,
+    required this.notes,
+  });
+
+  final List notes;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: notes.length,
+      itemBuilder: (context, index) {
+        final note = notes[index] as Map<String, dynamic>;
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        NoteDetailScreen(note: Note.fromFirestore(note))));
+          },
+          child: NoteWidget(note: Note.fromFirestore(note)),
+        );
+      },
+    );
+  }
+}
+
+class NoteGridView extends StatelessWidget {
+  const NoteGridView({
+    super.key,
+    required this.notes,
+  });
+
+  final List notes;
+
+  @override
+  Widget build(BuildContext context) {
+    return MasonryGridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 4,
+        crossAxisSpacing: 4,
+        scrollDirection: Axis.vertical,
+        itemCount: notes.length,
+        itemBuilder: (context, index) {
+          final note = notes[index] as Map<String, dynamic>;
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          NoteDetailScreen(note: Note.fromFirestore(note))));
+            },
+            child: NoteWidget(note: Note.fromFirestore(note)),
+          );
+        });
   }
 }
---
+++ lib/Screens/Home/widgets/note_widget.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/models/note.dart';
+
+class NoteWidget extends StatefulWidget {
+  final Note note;
+  const NoteWidget({super.key, required this.note});
+
+  @override
+  State<NoteWidget> createState() => _NoteWidgetState();
+}
+
+class _NoteWidgetState extends State<NoteWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Container(
+        padding: const EdgeInsets.all(4.0),
+        decoration: BoxDecoration(
+          //color: colors[widget.note.color],
+          borderRadius: const BorderRadius.all(
+            Radius.circular(15.0),
+          ),
+          border: Border.all(width: 2.0),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (widget.note.title.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  widget.note.title,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            if (widget.note.content.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  widget.note.content,
+                  style: const TextStyle(color: Colors.black),
+                ),
+              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    widget.note.dateCreated.toString(),
+                    style: const TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+            //   if (widget.note.reminder != null)
+            //     if (DateTime.parse(widget.note.reminder).isBefore(DateTime.now()))
+            //       Container(
+            //         decoration: BoxDecoration(
+            //           border: Border(
+            //             top: BorderSide(
+            //               color: Colors.black,
+            //               width: 1,
+            //             ),
+            //           ),
+            //         ),
+            //         child: Row(
+            //           // crossAxisAlignment: CrossAxisAlignment.start,
+            //           mainAxisAlignment: MainAxisAlignment.center,
+            //           children: [
+            //             InkWell(
+            //               onTap: () {
+            //                 print('object');
+            //               },
+            //               child: Padding(
+            //                 padding: const EdgeInsets.all(8.0),
+            //                 child: Text(
+            //                   'Mark As Done',
+            //                   style: TextStyle(
+            //                     fontWeight: FontWeight.bold,
+            //                     color: Colors.black,
+            //                   ),
+            //                 ),
+            //               ),
+            //             ),
+            //             Theme(
+            //               data: Theme.of(context).copyWith(
+            //                 unselectedWidgetColor: Colors.black,
+            //               ),
+            //               child: Checkbox(
+            //                 onChanged: (value) async {
+            //                   widget.note.markAsDone = value.toString();
+            //                   await dbHelper.updateNote(widget.note);
+            //                   setState(() {});
+            //                 },
+            //                 value: _markAsDone,
+            //                 activeColor: Colors.black,
+            //                 hoverColor: Colors.black,
+            //                 visualDensity: VisualDensity(vertical: 2.0),
+            //               ),
+            //             )
+            //           ],
+            //         ),
+            //       ),
+          ],
+        ),
+      ),
+    );
+  }
+}
